04681bda19f4d0adcef7163d3b26ad623eddc1d2
Sam Guymer <sam@guymer.me>
2016-12-26 19:15:19 +1000

    Update docs

--- docs.sh
+++ docs.sh
@@ -4,7 +4,7 @@ set -e
 bazel build //docs
 unzip -o bazel-bin/docs/docs-skydoc.zip
 echo '' > README.md
-cat container.md >> README.md
-cat pull.md >> README.md
-cat test.md >> README.md
-rm container.md pull.md test.md
+cat container/container.md >> README.md
+cat container/pull.md >> README.md
+cat container/test.md >> README.md
+rm container/container.md container/pull.md container/test.md index.md
